2222c79f2f998ca228ee4d7e36592e8cfae52713
Barry Carlyon <barry@barrycarlyon.co.uk>
2022-02-18 00:05:56 +0000

    add a "current state" badge

--- app/views/assets/script.js
+++ app/views/assets/script.js
@@ -140,6 +140,8 @@ function processExtension(extension_details) {
     }
 }
 function buildLayout(details) {
+    document.getElementById('state').textContent = details.state;
+
     document.getElementById('layout_icon').style.backgroundImage = `url(${details.icon_urls['100x100']})`;
 
     document.getElementById('layout_name').textContent = details.name;
